f0f2b4c3cbf7fd940c058db1c896f2cce20162cf
CBPaulS <paulchacon13@gmail.com>
2024-01-12 12:35:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sumativa3_CBPaulS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
